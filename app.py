5b189c70c9eab63650f9e49282c6a6900e82a830
Ilya Shulga <ilya.shulga@solaredge.com>
2023-05-31 23:19:43 +0300

    Plotting random live generated data

--- app.py
+++ app.py
@@ -3,25 +3,26 @@ import numpy as np # np mean, np random
 import pandas as pd # read csv, df manipulation
 import time # to simulate a real time data, time loop 
 import plotly.express as px # interactive charts 
-
+from datetime import datetime, timedelta
 
 # read csv from a github repo
-df = pd.read_csv("https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv")
+#df = pd.read_csv("https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv")
 
+df = pd.read_csv("measurements.csv")
 
 st.set_page_config(
-    page_title = 'Real-Time Data Science Dashboard',
+    page_title = 'Solaredge SenseConnect LIVE',
     page_icon = '✅',
     layout = 'wide'
 )
 
 # dashboard title
 
-st.title("Real-Time / Live Data Science Dashboard")
+st.title("SenseConnect LIVE")
 
 # top-level filters 
 
-job_filter = st.selectbox("Select the Job", pd.unique(df['job']))
+#job_filter = st.selectbox("Select the Job", pd.unique(df['job']))
 
 
 # creating a single-element container.
@@ -29,46 +30,64 @@ placeholder = st.empty()
 
 # dataframe filter 
 
-df = df[df['job']==job_filter]
+#df = df[df['job']==job_filter]
 
 # near real-time / live feed simulation 
 
 for seconds in range(200):
 #while True: 
-    
-    df['age_new'] = df['age'] * np.random.choice(range(1,5))
-    df['balance_new'] = df['balance'] * np.random.choice(range(1,5))
+    df = pd.read_csv("measurements.csv").tail(200)
+    #df['age_new'] = df['age'] * np.random.choice(range(1,5))
+    #df['balance_new'] = df['balance'] * np.random.choice(range(1,5))
+
+    # Take last measurement raw
+    #data_in = df.head(1)
 
     # creating KPIs 
-    avg_age = np.mean(df['age_new']) 
-
-    count_married = int(df[(df["marital"]=='married')]['marital'].count() + np.random.choice(range(1,30)))
+    power = df['Power (W)'].iloc[-1]
+    voltage = df['Voltage (V)'].iloc[-1]
+    temperature = df['Temperature (C)'].iloc[-1]
+    temperature_prev = df['Temperature (C)'].iloc[-2]
+    timestamp = df['Timestamp'].iloc[-1]
+    timestamp_formated = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+    print(timestamp)
+    print(power)
+    print(voltage)
+    #count_married = int(df[(df["marital"]=='married')]['marital'].count() + np.random.choice(range(1,30)))
     
-    balance = np.mean(df['balance_new'])
+    #balance = np.mean(df['balance_new'])
 
     with placeholder.container():
         # create three columns
         kpi1, kpi2, kpi3 = st.columns(3)
-
+        #kpi1, kpi2 = st.columns(2)
+        
         # fill in those three columns with respective metrics or KPIs 
-        kpi1.metric(label="Age ⏳", value=round(avg_age), delta= round(avg_age) - 10)
-        kpi2.metric(label="Married Count 💍", value= int(count_married), delta= - 10 + count_married)
-        kpi3.metric(label="A/C Balance ＄", value= f"$ {round(balance,2)} ", delta= - round(balance/count_married) * 100)
+        kpi1.metric(label="Power", value=f"{round(power)} W")
+        kpi2.metric(label="Voltage", value= f"{int(voltage)} V")
+        kpi3.metric(label="Connector Temperature", value= f"{round(temperature)} °C", delta= f"{round(temperature) - round(temperature_prev)} °C", delta_color="inverse")
 
         # create two columns for charts 
 
-        fig_col1, fig_col2 = st.columns(2)
+        fig_col1, fig_col2, fig_col3 = st.columns(3)
         with fig_col1:
             st.markdown("### First Chart")
-            fig = px.density_heatmap(data_frame=df, y = 'age_new', x = 'marital')
+            fig = px.line(data_frame = df, y = 'Power (W)', x = 'Timestamp')
+            fig.update_xaxes(range=[timestamp_formated - timedelta(seconds=30) , timestamp_formated  + timedelta(seconds=90)])
             st.write(fig)
         with fig_col2:
             st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame = df, x = 'age_new')
+            fig2 = px.line(data_frame = df, y = 'Voltage (V)', x = 'Timestamp')
+            fig2.update_xaxes(range=[timestamp_formated - timedelta(seconds=30) , timestamp_formated  + timedelta(seconds=90)])
             st.write(fig2)
+        with fig_col3:
+            st.markdown("### Third Chart")
+            fig3 = px.line(data_frame = df, y = 'Temperature (C)', x = 'Timestamp')
+            fig3.update_xaxes(range=[timestamp_formated - timedelta(seconds=30) , timestamp_formated  + timedelta(seconds=90)])
+            st.write(fig3)
         st.markdown("### Detailed Data View")
         st.dataframe(df)
-        time.sleep(1)
+        time.sleep(0.5)
     #placeholder.empty()
 
 
---
+++ generate_measurements.py
@@ -0,0 +1,49 @@
+import csv
+import random
+import time
+import os
+
+
+def generate_voltage():
+    """Simulates the generation of voltage values."""
+    return random.uniform(200, 240)  # Adjust the range as per your requirements
+
+def generate_temperature():
+    """Simulates the generation of voltage values."""
+    return random.uniform(50, 120)  # Adjust the range as per your requirements
+
+def calculate_power(voltage):
+    """Calculates the power based on the voltage value."""
+    return (voltage ** 2) / 1000  # Adjust the formula as per your requirements
+
+def save_to_csv(data, filename):
+    """Saves the data to a CSV file, overwriting if it already exists."""
+    file_exists = os.path.isfile(filename)
+    mode = 'a' if file_exists else 'w'  # Use 'w' mode only if the file doesn't exist
+    with open(filename, mode, newline='') as file:
+        writer = csv.writer(file)
+        if not file_exists and mode == 'w':  # Only write header if the file is newly created
+            writer.writerow(header)
+        if mode == 'a':
+            writer.writerow(data)
+
+# Configuration
+num_readings = 100  # Number of readings to generate
+delay = 0.5  # Delay in seconds between each reading
+output_file = 'measurements.csv'  # Output file name
+
+# Generate and save the measurements
+header = ['Timestamp', 'Voltage (V)', 'Power (W)', 'Temperature (C)']
+if not os.path.isfile(output_file):
+    save_to_csv(header, output_file)
+
+for _ in range(num_readings):
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+    voltage = generate_voltage()
+    power = calculate_power(voltage)
+    temperature = generate_temperature()
+    measurement = [timestamp, voltage, power, temperature]
+    save_to_csv(measurement, output_file)
+    time.sleep(delay)
+
+print(f"{num_readings} measurements saved to '{output_file}'")
