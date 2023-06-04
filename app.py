87b7ef31ea38131533324ba545f936d074732382
Ilya Shulga <ilya.shulga@solaredge.com>
2023-06-04 12:45:08 +0300

    Some design cosmetic improvements

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import time # to simulate a real time data, time loop
 import plotly.express as px # interactive charts 
 from datetime import datetime, timedelta
 import plotly.graph_objects as go
-from skimage import data
-#from streamlit_extras.let_it_rain import rain
+
+#import streamlit_extras.let_it_rain as rain
 
 # read csv from a github repo
 #df = pd.read_csv("https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv")
@@ -45,6 +45,7 @@ st.markdown(
 <style>
 [data-testid="stMetricValue"] {
     font-size: 60px;
+    text-align: center;
 }
 </style>
 """,
@@ -57,6 +58,20 @@ st.markdown(
 <style>
 [data-testid="stMetricLabel"] {
     font-size: 30px;
+    text-align: center;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+# Adjust the font of a st.metric component
+st.markdown(
+    """
+<style>
+[data-testid="stMetricDelta"] {
+    font-size: 30px;
+    text-align: center;
 }
 </style>
 """,
@@ -105,16 +120,39 @@ while(True):
     with placeholder.container():
         # create three columns
         #st.write('1 + 1 = ', 2)
+        if sense_connect:
+            #st.header(':red[SenseConnect event Detected!]')
+            st.markdown(""" <style> .big-font { text-align: center; background-color: red; font-size:32px; color: white; !important; } </style> """, unsafe_allow_html=True) 
+            st.markdown('<p class="big-font">SenseConnect event Detected!!!</p>', unsafe_allow_html=True)
+                
+            #st.write('SenseConnect event Detected!')
+        else:
+            st.markdown(""" <style> .big-font { text-align: center ;font-size:32px; color: black; } </style> """, unsafe_allow_html=True) 
+            st.markdown('<p class="big-font">Monitoring Optimizer Output Connector Temperature...</p>', unsafe_allow_html=True)
+            #st.write('Monitoring Optimizer Output Connector Temperature...')
         kpi1, kpi2, kpi3 = st.columns(3)
         sense_connect_column = st.columns(1)
         
-        
-        if sense_connect:
-            st.error('SenseConnect event Detected')
-        else:
-            st.info('Monitoring Optimizer Output Connector Temperature...')
+        #st.markdown(
+        #    """
+        #    <style>
+        #    [data-testid="stMarkdownContainerValue"] {
+        #        font-size: 50px;
+        #    }
+        #    </style>
+        #    """,
+        #    unsafe_allow_html=True
+        #    ) # This don't work
 
-                
+
+
+
+        #error = st.alert("This is error alert.", title="Error", type="error", icon="🚨")
+        
+        #if sense_connect:
+        #    error.open()
+        #else:
+        #    error.close()
         
 
         graph_column1, graph_column2, graph_column3 = st.columns(3)
@@ -132,8 +170,14 @@ while(True):
         with graph_column1:
             #st.markdown("### Optimizer Output Power")
             fig = px.line(data_frame = df, y = 'Power', x = 'Time')
-            fig.update_xaxes(range=[timestamp_formated - timedelta(seconds=90) , timestamp_formated  + timedelta(seconds=30)])
-            fig.update_yaxes(range=[0, 300])
+            fig.update_xaxes(range=[timestamp_formated - timedelta(seconds=90) , timestamp_formated  + timedelta(seconds=30)], title=dict(text='', font=dict(
+                size=32
+                )
+            ))
+            fig.update_yaxes(range=[0, 300], title=dict(text='Power (W)', font=dict(
+                size=32
+                )
+            ))
             st.write(fig)
             #img = data.astronaut()
             #fig4 = px.imshow(img, binary_format="jpeg", binary_compression_level=0)
@@ -144,14 +188,26 @@ while(True):
         with graph_column2:
             #st.markdown("### Optimizer Output Voltage")
             fig2 = px.line(data_frame = df, y = 'Vout', x = 'Time')
-            fig2.update_xaxes(range=[timestamp_formated - timedelta(seconds=90) , timestamp_formated  + timedelta(seconds=30)])
-            fig2.update_yaxes(range=[0, 50])
+            fig2.update_xaxes(range=[timestamp_formated - timedelta(seconds=90) , timestamp_formated  + timedelta(seconds=30)], title=dict(text='Time', font=dict(
+                size=32
+                )
+            ))
+            fig2.update_yaxes(range=[0, 30], title=dict(text='Voltage (V)', font=dict(
+                size=32
+                )
+            ))
             st.write(fig2)
         with graph_column3:
             #st.markdown("### MC4 Connector Temperature")
             fig3 = px.line(data_frame = df, y = 'thermistor', x = 'Time')
-            fig3.update_xaxes(range=[timestamp_formated - timedelta(seconds=90) , timestamp_formated  + timedelta(seconds=30)])
-            fig3.update_yaxes(range=[0, 160])
+            fig3.update_xaxes(range=[timestamp_formated - timedelta(seconds=90) , timestamp_formated  + timedelta(seconds=30)], title=dict(text='', font=dict(
+                size=32
+                )
+            ))
+            fig3.update_yaxes(range=[0, 160], title=dict(text='Temperature (°C)', font=dict(
+                size=32
+                )
+            ))
             st.write(fig3)
         #st.markdown("### Detailed Data View")
         #st.dataframe(df)
